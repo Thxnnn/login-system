03563e97d994c503418fe23e2a3d9d15f04da2cd
Thanwarat Munkam <mk.thanwarat@gmail.com>
2024-03-17 20:02:02 +0700

    forminput

--- client/src/contents/Register.jsx
+++ client/src/contents/Register.jsx
@@ -7,34 +7,67 @@ function Register() {
     }, []);
 
 
-    const [signupForm, setSignupFrom] = useState('')
-
-    const [username, setUsername] = useState('')
-    const [password, setPassword] = useState('')
-    const [confirmPassword, setConfirmPassword] = useState('')
-
-
+    const [signupForm, setSignupFrom] = useState({
+        username:"",
+        password:"",
+        confirmPassword:""
+    })
 
 
-
-    const handledata = (e) => {
-        e.preventDefault();
-        console.log('username:', username);
-        console.log('password:', password);
-        console.log('confirmPassword:', confirmPassword);
-    }
-
-    const handleUsernameChange = (e) => {
-        setUsername(e.target.value);
+    const handleChange = (e) => {
+        setSignupFrom({
+            ...signupForm,
+            [e.target.name]: e.target.value,
+        });
+        
     };
-    
 
-    const handlePasswordChange = (e) => {
-        setPassword(e.target.value);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const { username, password,confirmPassword } = signupForm;
+        console.log('Submitted data:', username, password,confirmPassword);
+        
+        if (password == confirmPassword) {
+            console.log("Passwords match");
+            const userdata = {
+                'username': username,
+                'password': password,
+                'confirmPassword': confirmPassword
+            }
+    
+            console.log(userdata)
+            
+            fetch('http://localhost:5000/signup', {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(userdata)
+            })
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+        } else {
+            console.log("Passwords don't match");
+            setSignupFrom(() => ({
+                ...signupForm,
+                password:'',
+                confirmPassword: ''
+            }));
+
+        }
+
+        
     }
-    const handleConfirmPasswordChange = (e) => {
-        setConfirmPassword(e.target.value);
-    }
+
+
+
+
 
 
 
@@ -47,90 +80,92 @@ function Register() {
                     </div>
                     <h1 className='text-center text-[30px] text-white font-bold'>Signup</h1>
 
-                    <form action="">
+                    <form onSubmit={handleSubmit}>
                         <div className="mt-10">
-                        
-                        <div className="relative mb-5">
-                            <input type="text" id="username"
-                                value={username} onChange={handleUsernameChange}
-                                className="peer py-4 px-0 block w-full bg-transparent border-t-transparent border-b-2 border-x-transparent border-b-gray-200 text-sm placeholder:text-transparent 
-                                ocus:border-t-transparent focus:border-x-transparent focus:border-b-gray-500 focus:ring-0 disabled:opacity-50 disabled:pointer-events-none 
-                                dark:border-b-gray-500 dark:text-gray-900 dark:focus:ring-gray-600 dark:focus:border-b-gray-600
-                                focus:outline-none
-                                focus:pt-10
-                                focus:pb-2
-                                [&:not(:placeholder-shown)]:pt-10
-                                [&:not(:placeholder-shown)]:pb-2
-                                autofill:pt-6
-                                autofill:pb-2" placeholder="********" />
-                            <label htmlFor="username" 
-                                className="absolute top-0 start-0 py-4 px-0 h-full text-md truncate pointer-events-none transition ease-in-out duration-100 border border-transparent 
-                                dark:text-white peer-disabled:opacity-50 peer-disabled:pointer-events-none
-                                peer-focus:text-md
-                                peer-focus:-translate-y-1.5
-                                peer-focus:text-white
-                                peer-[:not(:placeholder-shown)]:text-md
-                                peer-[:not(:placeholder-shown)]:-translate-y-1.5
-                                peer-[:not(:placeholder-shown)]:text-white"
-                            >Username</label>
-                        </div>
-                        <div className="relative mb-5">
-                            <input type="password" id="password" value={password} onChange={handlePasswordChange}
-                                className="peer py-4 px-0 block w-full bg-transparent border-t-transparent border-b-2 border-x-transparent border-b-gray-200 text-sm placeholder:text-transparent 
-                                ocus:border-t-transparent focus:border-x-transparent focus:border-b-gray-500 focus:ring-0 disabled:opacity-50 disabled:pointer-events-none 
-                                dark:border-b-gray-500 dark:text-gray-900 dark:focus:ring-gray-600 dark:focus:border-b-gray-600
-                                focus:outline-none
-                                focus:pt-10
-                                focus:pb-2
-                                [&:not(:placeholder-shown)]:pt-10
-                                [&:not(:placeholder-shown)]:pb-2
-                                autofill:pt-6
-                                autofill:pb-2" placeholder="********" />
-                            <label htmlFor="password" 
-                                className="absolute top-0 start-0 py-4 px-0 h-full text-md truncate pointer-events-none transition ease-in-out duration-100 border border-transparent 
-                                dark:text-white peer-disabled:opacity-50 peer-disabled:pointer-events-none
-                                peer-focus:text-md
-                                peer-focus:-translate-y-1.5
-                                peer-focus:text-white
-                                peer-[:not(:placeholder-shown)]:text-md
-                                peer-[:not(:placeholder-shown)]:-translate-y-1.5
-                                peer-[:not(:placeholder-shown)]:text-white"
-                            >Password</label>
-                        </div>
-                        <div className="relative mb-5">
-                            <input type="password" id="confirmpassword" value={confirmPassword} onChange={handleConfirmPasswordChange}
-                                className="peer py-4 px-0 block w-full bg-transparent border-t-transparent border-b-2 border-x-transparent border-b-gray-200 text-sm placeholder:text-transparent 
-                                ocus:border-t-transparent focus:border-x-transparent focus:border-b-gray-500 focus:ring-0 disabled:opacity-50 disabled:pointer-events-none 
-                                dark:border-b-gray-500 dark:text-gray-900 dark:focus:ring-gray-600 dark:focus:border-b-gray-600
-                                focus:outline-none
-                                focus:pt-10
-                                focus:pb-2
-                                [&:not(:placeholder-shown)]:pt-10
-                                [&:not(:placeholder-shown)]:pb-2
-                                autofill:pt-6
-                                autofill:pb-2" placeholder="********" />
-                            <label htmlFor="confirmpassword" 
-                                className="absolute top-0 start-0 py-4 px-0 h-full text-md truncate pointer-events-none transition ease-in-out duration-100 border border-transparent 
-                                dark:text-white peer-disabled:opacity-50 peer-disabled:pointer-events-none
-                                peer-focus:text-md
-                                peer-focus:-translate-y-1.5
-                                peer-focus:text-white
-                                peer-[:not(:placeholder-shown)]:text-md
-                                peer-[:not(:placeholder-shown)]:-translate-y-1.5
-                                peer-[:not(:placeholder-shown)]:text-white"
-                            >Confirm password</label>
-                        </div>
 
-                        <div className="">
-                            <button className='w-full border border-1 p-2 rounded-md hover:bg-transparent hover:blur-xs text-lg font-bold'
-                                onClick={handledata}
-                            >
-                                Signup
-                            </button>
+                            <div className="relative mb-5">
+                                <input type="text" id="username" name="username"
+                                    value={signupForm.username} onChange={handleChange}
+                                    className="peer py-4 px-0 block w-full bg-transparent border-t-transparent border-b-2 border-x-transparent border-b-gray-200 text-sm placeholder:text-transparent 
+                                ocus:border-t-transparent focus:border-x-transparent focus:border-b-gray-500 focus:ring-0 disabled:opacity-50 disabled:pointer-events-none 
+                                dark:border-b-gray-500 dark:text-gray-900 dark:focus:ring-gray-600 dark:focus:border-b-gray-600
+                                focus:outline-none
+                                focus:pt-10
+                                focus:pb-2
+                                [&:not(:placeholder-shown)]:pt-10
+                                [&:not(:placeholder-shown)]:pb-2
+                                autofill:pt-6
+                                autofill:pb-2" placeholder="********" />
+                                <label htmlFor="username"
+                                    className="absolute top-0 start-0 py-4 px-0 h-full text-md truncate pointer-events-none transition ease-in-out duration-100 border border-transparent 
+                                dark:text-white peer-disabled:opacity-50 peer-disabled:pointer-events-none
+                                peer-focus:text-md
+                                peer-focus:-translate-y-1.5
+                                peer-focus:text-white
+                                peer-[:not(:placeholder-shown)]:text-md
+                                peer-[:not(:placeholder-shown)]:-translate-y-1.5
+                                peer-[:not(:placeholder-shown)]:text-white"
+                                >Username</label>
+                            </div>
+                            <div className="relative mb-5">
+                                <input type="password" id="password" name="password"
+                                value={signupForm.password} onChange={handleChange}
+                                    className="peer py-4 px-0 block w-full bg-transparent border-t-transparent border-b-2 border-x-transparent border-b-gray-200 text-sm placeholder:text-transparent 
+                                ocus:border-t-transparent focus:border-x-transparent focus:border-b-gray-500 focus:ring-0 disabled:opacity-50 disabled:pointer-events-none 
+                                dark:border-b-gray-500 dark:text-gray-900 dark:focus:ring-gray-600 dark:focus:border-b-gray-600
+                                focus:outline-none
+                                focus:pt-10
+                                focus:pb-2
+                                [&:not(:placeholder-shown)]:pt-10
+                                [&:not(:placeholder-shown)]:pb-2
+                                autofill:pt-6
+                                autofill:pb-2" placeholder="********" />
+                                <label htmlFor="password"
+                                    className="absolute top-0 start-0 py-4 px-0 h-full text-md truncate pointer-events-none transition ease-in-out duration-100 border border-transparent 
+                                dark:text-white peer-disabled:opacity-50 peer-disabled:pointer-events-none
+                                peer-focus:text-md
+                                peer-focus:-translate-y-1.5
+                                peer-focus:text-white
+                                peer-[:not(:placeholder-shown)]:text-md
+                                peer-[:not(:placeholder-shown)]:-translate-y-1.5
+                                peer-[:not(:placeholder-shown)]:text-white"
+                                >Password</label>
+                            </div>
+                            <div className="relative mb-5">
+                                <input type="password" id="confirmPassword" name="confirmPassword" 
+                                    value={signupForm.confirmPassword} onChange={handleChange}
+                                    className="peer py-4 px-0 block w-full bg-transparent border-t-transparent border-b-2 border-x-transparent border-b-gray-200 text-sm placeholder:text-transparent 
+                                ocus:border-t-transparent focus:border-x-transparent focus:border-b-gray-500 focus:ring-0 disabled:opacity-50 disabled:pointer-events-none 
+                                dark:border-b-gray-500 dark:text-gray-900 dark:focus:ring-gray-600 dark:focus:border-b-gray-600
+                                focus:outline-none
+                                focus:pt-10
+                                focus:pb-2
+                                [&:not(:placeholder-shown)]:pt-10
+                                [&:not(:placeholder-shown)]:pb-2
+                                autofill:pt-6
+                                autofill:pb-2" placeholder="********" />
+                                <label htmlFor="confirmPassword"
+                                    className="absolute top-0 start-0 py-4 px-0 h-full text-md truncate pointer-events-none transition ease-in-out duration-100 border border-transparent 
+                                dark:text-white peer-disabled:opacity-50 peer-disabled:pointer-events-none
+                                peer-focus:text-md
+                                peer-focus:-translate-y-1.5
+                                peer-focus:text-white
+                                peer-[:not(:placeholder-shown)]:text-md
+                                peer-[:not(:placeholder-shown)]:-translate-y-1.5
+                                peer-[:not(:placeholder-shown)]:text-white"
+                                >Confirm password</label>
+                            </div>
+
+                            <div className="">
+                                <button className='w-full border border-1 p-2 rounded-md hover:bg-transparent hover:blur-xs text-lg font-bold'
+                                    type='submit'
+                                >
+                                    Signup
+                                </button>
+                            </div>
                         </div>
-                    </div> 
                     </form>
-                    
+
                 </div>
             </div>
         </>
--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 var cors = require('cors')
 const mysql = require('mysql');
-
+// var bodyParser = require('body-parser')
 const app = express();
 
+// app.use(bodyParser.json());
+app.use(express.json());
 app.use(cors())
 
 const db = mysql.createConnection({
@@ -19,6 +21,39 @@ db.connect((err) => {
     }
     console.log('MySQL Connected...');
 });
+
+app.get('/', (req, res) => {
+    res.send('Hello World')
+})
+
+
+app.post('/signup', (req, res) => {
+    const data = req.body;
+    const { username, password, confirmPassword } = req.body;
+    console.log(data)
+    console.log('Username:', username);
+    console.log('Password:', password);
+    console.log('Confirm Password:', confirmPassword);
+
+    if (password !== confirmPassword) {
+        return res.status(400).json({ error: "Passwords do not match" });
+    }
+
+    const sql = "INSERT INTO users (username, password) VALUES (?, ?)";
+    db.query(sql, [username, password], (err, result) => {
+        if (err) {
+            console.error("Error inserting data:", err);
+            return res.status(500).json({ error: "Internal server error" });
+        }
+        console.log("Data inserted successfully");
+        res.status(200).json({ message: "Signup successful" });
+    });
+
+})
+
+app.post('/signin', (req,res) => {
+    res.statusCode(200)
+})
 
 
 
